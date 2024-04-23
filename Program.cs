e32a7db6feb32334b43dcf7261fc9d33cc3b52c4
Aliakbar025 <aliakber.huseynov555@gmail.com>
2024-04-23 15:07:39 +0300

    Algorithm

---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using Microsoft.VisualBasic;
+
+//My First Task.
+//Console.WriteLine("Enter 5 numbers");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//int number4 = Convert.ToInt32(Console.ReadLine());
+//int number5 = Convert.ToInt32(Console.ReadLine());
+
+//int Result = number1 + number2 + number3 + number4 + number5;
+
+//Console.WriteLine("The result is " + Result);
+
+//My Second Task.
+//Console.WriteLine("Input a number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int result = number * number;
+//Console.WriteLine("The result is " + result);
+
+
+//My Third Task.
+//Console.WriteLine("input a number");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("input a second number");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("input a third number");
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2 && number1 > number3)
+//{
+//    Console.WriteLine("number1 is more than number2 and number3");
+//}
+//if (number2 > number1 && number2 > number3)
+//{
+//    Console.WriteLine("number2 is more than number1 and number3");
+//}
+//if (number3 > number1 && number3 > number2)
+//{
+//    Console.WriteLine("number3 is more than number1 and number2");
+//}
+//Console.WriteLine("the biggest number among three inputed numbers");
+
+//My Fourth Task.
+
+//Console.WriteLine("Enter number1");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter number2");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 * number1 * number1 > number2)
+//{
+//    Console.WriteLine("the cube of num1 is greater than num2");
+//}
+//else
+//{
+//    Console.WriteLine("the cube of num1 is not greater than num2");
+//}
+//Console.WriteLine("Determination that shows the bigger or not bigger than num2 , num1's cube");
+
+////My Fifth Task.
+
+//Console.WriteLine("Input a number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number % 3 == 0 && number % 5 == 0)
+//{
+//    Console.WriteLine("Number can be divided by 3 and 5");
+//}
+//else
+//{
+//    Console.WriteLine("Number cannot be divided by 3 and 5");
+//}
+//Console.WriteLine("The division of the number by 3 and by 5");
+
+//////My Sixth Task.
+//Console.WriteLine("Enter the amount of money in the credit card");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter the price of the product");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 >= number2)
+//{
+//    Console.WriteLine("your purchase is approved");
+//}
+//else if (number1 < number2)
+//{
+//    Console.WriteLine("you have not enough credits ");
+//}
+//Console.WriteLine("Determination that shows us is there enough credits in debit card to purchase product");
+
+
+
+//My Seventh Task.
+
+
+//Console.WriteLine("Input the result of the first exam");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input the result of the second exam");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input the result of the third exam");
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input the result of the fourth exam");
+//int number4 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input the result of the fifth exam");
+//int number5 = Convert.ToInt32(Console.ReadLine());
+//if ((number1 + number2 + number3 + number4 + number5) / 5 > 65)
+
+//{
+//    Console.WriteLine("You have succesfully passed the semester");
+//}
+//else
+//{
+//    Console.WriteLine("You have not passed the semester");
+//}
+//Console.WriteLine("Determination of passing or not the semester");
+
+
+//My Eights Task.
+
+//Console.WriteLine("Input natural number1");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input natural number2");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2)
+//{
+//    Console.WriteLine("number1 is bigger than number2");
+//}
+//else if (number1 < number2)
+//{
+//    Console.WriteLine("number2 is bigger than number1");
+//}
+//else
+//{
+//    Console.WriteLine("number1 is equivalent to number2");
+//}
+//Console.WriteLine("Determination of which  of the given natural number is more than the other one");
+
+
+//My Ninth Task.
+
+
+//Console.WriteLine("Input a temperature");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number < 0)
+//{
+//    Console.WriteLine("temperature is under the temperature of freezing water");
+//}
+//else if (number > 0)
+//{
+//    Console.WriteLine("temperature is above the temperature of freezing water");
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
+//Console.WriteLine("Determination that shows is the given temperature under or above the temperature of freezing water");
+
+
+//My Tenth Task.
+
+//Console.WriteLine("Enter a number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number > 0)
+//{
+//    Console.WriteLine("a number is positive");
+//}
+//else if (number < 0)
+//{
+//    Console.WriteLine("a number is negative");
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
+//Console.WriteLine("Determination that shows us either,number is positive,or number is negative");
+
+//My 11th task.
+
+//for (int i = 0; i < 50; i++)
+//{
+//    Console.WriteLine(i + 1);
+//}
+
+//int i;
+//for (i=1;i<+100;i++)
+//{
+//    Console.WriteLine(i);
+//    if (i%3==0&&i%7==0)
+//    { Console.WriteLine(i++); }
+//}
